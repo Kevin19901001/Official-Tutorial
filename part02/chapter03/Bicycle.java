c1c822dda4677719d923e93115ef2b774b860918
Kevin <huanqing19901001@163.com>
2024-03-28 12:50:46 +0800

    Create Bicycle.java

---
+++ part02/chapter03/Bicycle.java
@@ -0,0 +1,19 @@
+/**
+ * 定义一个Bicycle类
+ * 
+ * @Author FanHuanQing
+ * @Since 2024-03-28
+ *
+ */
+public class Bicycle {
+
+    // Bicycle类有3个属性
+    public int cadence;
+    public int geer;
+    public int speed;
+
+    public static void main(String[] args) {
+
+    }
+    
+}
