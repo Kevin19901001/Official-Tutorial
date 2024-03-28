2cf53152375d20ba3444fb89d0295d59d77fb646
Kevin <huanqing19901001@163.com>
2024-03-28 12:57:48 +0800

    Update Bicycle.java

--- part02/chapter03/Bicycle.java
+++ part02/chapter03/Bicycle.java
@@ -7,11 +7,21 @@
  */
 public class Bicycle {
 
-    // Bicycle类有3个属性
-    public int cadence;
-    public int geer;
-    public int speed;
+    // Bicycle类定义3个属性
+    public int cadence;    // 节奏
+    public int gear;    // 传动装置
+    public int speed;    // 速度
 
+    // Bicycle类定义1个构造器
+    public Bicycle(int startCadence, int startSpeed, int startGear) {
+        gear = startGear;
+        cadence = startCadence;
+        speed = startSpeed;
+    }
+
+    // Bicycle类定义4个方法
+    
+    
     public static void main(String[] args) {
 
     }
