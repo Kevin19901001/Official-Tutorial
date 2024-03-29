a8791b507ceca64c0b40ac8bc032ac1237ca24eb
Kevin <huanqing19901001@163.com>
2024-03-29 12:39:01 +0800

    Create MyClass.java

---
+++ part02/chapter03/MyClass.java
@@ -0,0 +1,12 @@
+package part02.chapter03;
+
+/**
+ * 定义一个类的同时，继承父类并实现接口。
+ * 
+ * @author FanHuanQing
+ * @since 2024-03-29
+ *
+ */
+public MyClass extends MySuperClass implements YourInterface {
+
+  }
