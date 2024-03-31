33e3f0390188961ec948107422a15475f1e10fb3
huanqing19901001@163.com <huanqing19901001@163.com>
2024-03-31 17:44:15 +0800

    Update files.

--- part02/chapter03/Calculator.java
+++ part02/chapter03/Calculator.java
@@ -8,9 +8,22 @@ package part02.chapter03;
  */
 public class Calculator {
 
-  // 一个典型的方法定义
-  public double calculateAnswer(double wingSpan, int numberOfEngines, double length, double grossTons){
+	// 3.1.3 方法定义
+	// 定义方法需要的要素包括：方法返回类型、方法名、圆括号、花括号
+	
+	// 通常，方法的定义有6个组成部分：
+	// 1、public、private这样的修饰符；
+	// 2、返回类型，如果返回类型为空，则返回类型为void；
+	// 3、遵守规范的方法名；
+	// 4、圆括号中的参数列表，如果没有参数，那么圆括号中不需要任何内容；
+	// 5、一场列表，后面将会学习到；
+	// 6、方法体，方法的实现，包括局部变量的定义等等
+	
+	// 一个典型的方法定义
+	public double calculateAnswer(double wingSpan, int numberOfEngines, double length, double grossTons) {
 
-  }
-  
+		return grossTons;
+
+	}
+
 }
