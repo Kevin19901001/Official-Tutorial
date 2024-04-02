bc3167d2f609c3a21b207d34c78a07b78329b54a
Kevin <huanqing19901001@163.com>
2024-04-02 20:52:21 +0800

    Update DataArtist.java

--- part02/chapter03/DataArtist.java
+++ part02/chapter03/DataArtist.java
@@ -9,6 +9,20 @@ package part02.chapter03;
  */
 public class DataArtist {
 
-    
+    public void draw(String s){
+
+    }
+
+    public void draw(int i){
+
+    }
+
+    public void draw(char c}{
+
+    }
+
+    public void draw(double d, int i){
+
+    }
   
 }
