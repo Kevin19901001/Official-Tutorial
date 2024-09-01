0d3a70ead7666063c2669c8588bc527886b1840e
huanqing19901001@163.com <huanqing19901001@163.com>
2024-09-01 21:44:22 +0800

    Add a new file.

---
+++ part02/chapter03/Circle.java
@@ -0,0 +1,20 @@
+package part02.chapter03;
+
+/**
+ * @author FanHuanQing
+ * 
+ * @since 2024-09-01
+ * 
+ * 参数名称
+ * 
+ */
+public class Circle {
+
+	@SuppressWarnings("unused")
+	private int x, y, radius;
+	
+	public void setOrigin(int x, int y) {
+		
+	}
+	
+}
